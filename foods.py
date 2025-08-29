97d0179425cd62bc0ef4254f9a5896b4e1bce6e1
evgenProger <ivanov.evgen.v@gmail.com>
2025-08-29 13:17:58 +0300

    add choose foods

--- foods.py
+++ foods.py
@@ -1,4 +1,4 @@
-food = ["бургер",'пааста карбонара','суп овощной']
+food = ["бургер",'паста карбонара','суп овощной']
 salads = ['цезарь','оливье','овощной','салат под шубой']
 drinks = ['кока кола','фанта','газ.вода']
 print('основные блюда:')
@@ -6,15 +6,24 @@ count = 1
 for i in food:
     print('{}.{}'.format(count,i))
     count += 1
+foods = input('выбирите основное блюдо: ')
 print('-----------------')
 print('салаты:')
 count = 1
 for d in salads:
     print('{}.{}'.format(count,d))
     count += 1
+salad = input('выбирите салат: ')
 print('-----------------')
 print('напитки:')
 count = 1
 for a in drinks:
     print('{}.{}'.format(count,a))
     count += 1
+drink = input('выбирите ваш напиток: ')
+print('-----------------')
+print('ваш заказ:')
+print('основное блюдо: {}'.format(food[int(foods) - 1]))
+print('салат: {}'.format(salads[int(salad) - 1]))
+print('напиток: {}'.format(drinks[int(drink) - 1]))
+
