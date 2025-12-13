848df5d41bd194f2e8e93d1e8b594266a22f2661
Artemcoolprogger <Ivanov.Artem.e.2012@gmail.com>
2025-12-13 11:03:40 +0300

    refactor(change main code by one function)

--- foods.py
+++ foods.py
@@ -1,29 +1,37 @@
+
+def menu(choices, title, quest):
+   print(title)
+   count = 1
+   for i in choices:
+       print('{}.{}'.format(count,i))
+       count += 1
+   choice = input(quest)
+   print('-------------')
+   return choices[int(choice) - 1]
+
+
 food = ["бургер",'паста карбонара','суп овощной']
 salads = ['цезарь','оливье','овощной','салат под шубой']
 drinks = ['кока кола','фанта','газ.вода']
-print('основные блюда:')
-count = 1
-for i in food:
-    print('{}.{}'.format(count,i))
-    count += 1
-foods = input('выбирите основное блюдо: ')
-print('-----------------')
-print('салаты:')
-count = 1
-for d in salads:
-    print('{}.{}'.format(count,d))
-    count += 1
-salad = input('выбирите салат: ')
-print('-----------------')
-print('напитки:')
-count = 1
-for a in drinks:
-    print('{}.{}'.format(count,a))
-    count += 1
-drink = input('выбирите ваш напиток: ')
-print('-----------------')
+main_food =  menu(food, 'основные блюда:', 'выберите основное блюдо: ')
+my_salad = menu(salads, 'салаты:', 'выберите салат: ')
+my_drink = menu(drinks, 'напитки:', 'выберите напиток: ')
 print('ваш заказ:')
-print('основное блюдо: {}'.format(food[int(foods) - 1]))
-print('салат: {}'.format(salads[int(salad) - 1]))
-print('напиток: {}'.format(drinks[int(drink) - 1]))
+print('основное блюдо: {}'.format(main_food))
+print('салаты: {}'.format(my_salad))
+print('напиток: {}'.format(my_drink))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
