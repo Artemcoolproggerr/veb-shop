8c4976c9f7c67e4806ab65cb54ae2d94c7904fae
Artemcoolprogger <Ivanov.Artem.e.2012@gmail.com>
2025-12-30 13:42:40 +0300

    add validate enter of choose

--- foods.py
+++ foods.py
@@ -5,9 +5,22 @@ def menu(choices, title, question):
    for i in choices:
        print('{}.{}'.format(count,i))
        count += 1
+   print('для того чтобы сделать заказ введите номер блюда')
    choice = input(question)
    print('-' * len(question))
-   return choices[int(choice) - 1]
+   while True:
+       allowed_answers = []
+       for i in range(1, len(choices) + 1):
+           allowed_answers.append(str(i))
+       if choice in allowed_answers:
+           answer = choices[int(choice) - 1]
+           break
+       else:
+           print('НЕккорктный ввод!введите число от 1 до {}'.format(len(choices)))
+           choice = input(question)
+   return answer
+
+
 
 
 food = ["бургер",'паста карбонара','суп овощной']
