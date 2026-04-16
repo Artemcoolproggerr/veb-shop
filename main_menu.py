d83b6609d13ea9538eb91230efaa190bd26192d6
Artemcoolprogger <Ivanov.Artem.e.2012@gmail.com>
2026-04-16 18:45:01 +0300

    refactor(simplified the code and made a loop)

--- main_menu.py
+++ main_menu.py
@@ -2,7 +2,17 @@ import foods
 
 def main():
     order = foods.get_order()
-    print(order)
+    # two = order.get('main food')
+    # three = order.get('salad')
+    # four = order.get('drink')
+    # print('{} ваш заказ: '.format(one))
+    # print('основное блюдо: {}'.format(two))
+    # print('салаты: {}'.format(three))
+    # print('напиток: {}'.format(four))
+    for key, value in order.items():
+        print(f"{key}: {value}")
+    print('Итого:-------------')
+
 
 
 if __name__== "__main__":
